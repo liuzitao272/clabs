167e6534e2483d5d6e88d38cb3cbf5dbd85123cf
lzt272 <1085357101@qq.com>
2019-10-26 20:11:25 -0700

    added a file

--- lab1/lab2/bc.c
+++ lab1/lab2/bc.c
@@ -2,16 +2,15 @@
 #include<math.h>
 int main()
 {
-    double pow (double x double i);
-    int n,count=0,x,i;
+  int n,count=0,x,i;
     scanf("%d",&n);
     for(i=1;i<=10;i++){
          x=n%2;
-         n=(int)(n/pow(2,i));
+         n=(int)(n/2);
          if(x==1){
              count=count+1;
          }
     }
-    printf("%d,count");
+    printf("%d",count);
     return 0;
 }
