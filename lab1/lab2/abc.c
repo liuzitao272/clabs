913da4df34ec15deab7f8721b25fec36f1945434
lzt272 <1085357101@qq.com>
2019-11-01 01:45:42 -0700

    added a file

---
+++ lab1/lab2/abc.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+int main()
+{
+     unsigned int a=1;
+     unsigned int c=0 -1;
+     int b=0;
+     while(a !=0){ 
+         a=a<<1;
+         b=b+1;
+     }
+     printf("%d",b);
+     printf("%u\n",c);
+     return 0;
+}
