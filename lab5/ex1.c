8137aa0b9f5b56a7bc8e4aa98bd6644521a2196a
lzt272 <1085357101@qq.com>
2019-12-05 03:52:42 -0800

    added a file

---
+++ lab5/ex1.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+void reverse(char s[],int i,int len)
+{
+   int c,j;
+ 
+   j = len - (i + 1);
+ 
+   if(i < j)
+   {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+ 
+        reverse(s,++i,len);
+   }
+}
+ 
+int main()
+{
+    char s[100] = "liuzitao";
+ 
+    reverse(s,0,strlen(s));
+ 
+    printf("%s\n",s);
+ 
+    return 0;
+ 
+}
+
