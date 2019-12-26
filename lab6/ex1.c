ca1b7c1be0252dadac45683ba05da078eba4fd57
lzt272 <1085357101@qq.com>
2019-12-25 20:00:30 -0800

    added a file

--- lab6/ex1.c
+++ lab6/ex1.c
@@ -1,91 +1,79 @@
- #include <stdio.h>
- char getch(int number,int *recorder[]);
-void ungetch(int unumber,int *urecorder[],char ubeforeinput);
-void calulate(char input,int cnumber,int *crecorder[]);
- int main(int argc,int *argv[]){
- 	char c;
- 	int nowinput;
- 	while((nowinput=getch(argc,*argv[]))!='\0'){
- 		calulate(nowinput,argc,*argv[]);
- 		nowinput=getch(argc,*argv[]);
- 		if((c=getch(argc,*argv[]))=='\0'){
- 			ungetch(argc,*argv[],nowinput);
- 			break;
- 		}else{
- 			ungetch(argc,*argv[],c);
- 			ungetch(argc,*argv[],nowinput);
- 		}
- 	}
-	 printf("the answer is%d\n",getch(argc,*argv[]));
-	 return 0;
- 	
- }
- static int i=0,j=-1;
- char getch(int number,int *recorder[]){
- 	char m;
- 	recorder++;
- 	if(number==1){
- 		return (*recorder)[i--];
- 		recorder--;
- 	}else{
- 		
- 		m=(*++recorder)[j--];
-		 recorder=recorder-2;
-		 return m;
- 	}	
- }
- void ungetch(int unumber,int *urecorder[],char ubeforeinput){
- 	number=2;
- 	urecorder++;
- 	(*urecorder++)[++j]=beforinput;
- }
- 
- void calulate(char input,int cnumber,int *crecorder[]){
- 	
- 	void realcalulate(char input);
- 	
- 	if(!isdigit(input)){
- 		realcalulate(input)
- 		}
- 	else{
- 		ungetch(input);
- 		int temp;
- 		crecorder++;
- 		while(isdigit(temp=*crecorder[i++])){
- 			ungetch(crecorder,*crecorder[],temp);
- 		}
- 		i--;
-		realcalulate(temp);	
-		crecorder--;
- 	}
-	  
- }
-void realcalulate(char c){
-	char getch(int number,int *recorder[]);
-	char op1,op2;
-	switch(c){
- 			case *:
- 				ungetch(getch()*getch());
- 				break;
- 			case +:
- 				ungetch(getch()+getch());
- 				break;
- 			case -:
- 				op2=getch();
- 				op1=getch();
- 				ungetch(op1-op2);
- 				break;
- 			case /:
- 				op2=getch();
- 				op1=getch();
- 				if(op2!=0){
- 					ungetch(op1/op2);
- 				}else{
- 					printf("cann't divide 0\n");
- 				}
- 				break;
- 			default:
- 				printf("there is other  signal\n");
- 				break;
- 	}
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define MAXOP 100
+#define NUMBER '0'
+int getop(char s[]);
+void push(double);
+double pop(void);
+char s[MAXOP];
+main(int argc, char *argv[])
+{
+    int type;
+    double op2;
+    int j = 1;
+    int a = argc;
+    while((argc--)>0 && j<a){
+    type = getop(argv[j++]);
+        switch (type)
+        {
+        case NUMBER:
+            push(atof(argv[j-1]));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0)
+                push(pop() / op2);
+            else
+                printf("error:zero divisor\n");
+            break;
+        default:
+            printf("error:unknown command %s\n", s);
+            argc=1;
+            break;
+        }
+    }
+    printf("\t%.8g\n",pop());
+    return 0;
+}
+#define MAXVAL 100
+int sp = 0;
+double val[MAXVAL];
+void push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+    {
+        printf("error:stack full,can't push %g\n", f);
+    }
+}
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else
+    {
+        printf("error:stack empty\n");
+        return 0.0;
+    }
+}
+
+int getop(char s[])
+{
+    if (*s> '9' || *s < '0')
+        return *s;
+    else
+        return NUMBER;
+        
 }
